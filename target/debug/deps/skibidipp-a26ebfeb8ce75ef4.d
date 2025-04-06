cfb4c758b8c6f23cd1eacff4d4031c094d45c59f
Andrin <andrinrenggli@outlook.com>
2025-04-06 18:16:11 +0200

    Remove all build artifacts from tracking and update gitignore

--- target/debug/deps/skibidipp-a26ebfeb8ce75ef4.d
+++
@@ -1,9 +0,0 @@
-/home/andrin/school/personal/skibidipp/target/debug/deps/skibidipp-a26ebfeb8ce75ef4: src/main.rs src/lexer.rs src/parser.rs src/ast.rs src/codegen.rs
-
-/home/andrin/school/personal/skibidipp/target/debug/deps/skibidipp-a26ebfeb8ce75ef4.d: src/main.rs src/lexer.rs src/parser.rs src/ast.rs src/codegen.rs
-
-src/main.rs:
-src/lexer.rs:
-src/parser.rs:
-src/ast.rs:
-src/codegen.rs:
--- target/debug/skibidipp.d
+++
@@ -1,1 +0,0 @@
-/home/andrin/school/personal/skibidipp/target/debug/skibidipp: /home/andrin/school/personal/skibidipp/src/ast.rs /home/andrin/school/personal/skibidipp/src/codegen.rs /home/andrin/school/personal/skibidipp/src/lexer.rs /home/andrin/school/personal/skibidipp/src/main.rs /home/andrin/school/personal/skibidipp/src/parser.rs
