e343d7107495927723ad4a72fb6c9867dbf43ed0
Andrin <andrinrenggli@outlook.com>
2025-04-06 19:43:22 +0200

    added variables(pain in the ass)

--- build/out.asm
+++ build/out.asm
@@ -1,27 +1,24 @@
 section .data
-msg0 db "Hello World!", 10, 0
-msg1 db "test2", 10, 0
+str_0: db 'Hello Word!',0
 
 section .text
 global _start
 
 _start:
-    ; Print: Hello World!
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; stdout
-    mov rsi, msg0
-    mov rdx, 13
-    syscall
+push rbp
+mov rbp, rsp
+mov rax, 1
+mov rdi, 1
+mov rsi, str_0
+mov rdx, 11
+syscall
+mov rax, 0
+mov rdi, rax
+mov rax, 60
+syscall
 
-    ; Exit program
-    mov rax, 60         ; sys_exit
-    mov rdi, 10
-    syscall
-
-    ; Print: test2
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; stdout
-    mov rsi, msg1
-    mov rdx, 6
-    syscall
-
+mov rsp, rbp
+pop rbp
+mov rax, 60
+mov rdi, 0
+syscall
