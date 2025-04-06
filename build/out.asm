3481e8adf7b3a168613002651b86e7dd4cf843ae
Andrin <andrinrenggli@outlook.com>
2025-04-06 18:06:09 +0200

    fixed print finally

--- build/out.asm
+++ build/out.asm
@@ -1,7 +1,27 @@
+section .data
+msg0 db "Hello World!", 10, 0
+msg1 db "test2", 10, 0
+
 section .text
-    global _start
+global _start
 
 _start:
-    mov rax, 60            ; syscall number for exit
-    mov rdi, 10            ; exit code
+    ; Print: Hello World!
+    mov rax, 1          ; sys_write
+    mov rdi, 1          ; stdout
+    mov rsi, msg0
+    mov rdx, 13
     syscall
+
+    ; Exit program
+    mov rax, 60         ; sys_exit
+    mov rdi, 10
+    syscall
+
+    ; Print: test2
+    mov rax, 1          ; sys_write
+    mov rdi, 1          ; stdout
+    mov rsi, msg1
+    mov rdx, 6
+    syscall
+
