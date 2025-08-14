ee1b1e7dc64eab882facc0c5276dcec56adf3cea
Stiwyy <andrinrenggli@outlook.com>
2025-08-14 22:08:16 +0200

    prepare for more operaters and enhanced constant systestem

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,9 @@
-#[derive(Debug)]
 pub enum Expr {
     Number(i32),
+    Float(f64),
+    Boolean(bool),
     StringLiteral(String),
+    Array(Vec<Box<Expr>>),
     BinaryOp {
         op: BinOp,
         left: Box<Expr>,
@@ -10,16 +12,22 @@ pub enum Expr {
     Print(Box<Expr>),
     Exit(Box<Expr>),
     Const {
-            name: String,
-            value: Box<Expr>,
-        },
-        Variable(String),
+        name: String,
+        value: Box<Expr>,
+    },
+    Variable(String),
+    Null,
 }
 
-#[derive(Debug)]
 pub enum BinOp {
     Add,
     Sub,
     Mul,
     Div,
+    Equal,    // ==
+    NotEqual, // !=
+    Lt,       // <
+    Gt,       // >
+    Lte,      // <=
+    Gte,      // >=
 }
